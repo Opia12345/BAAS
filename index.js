959fdcf46552efb524b9ec5e785c5a40f741205e
Opia Precious <preciousopia7@gmail.com>
2024-06-28 21:57:56 +0100

    Change

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ mongoose
     console.log(err.message);
   });
 
-//ALL ROUTES
+//ROUTES
 app.post("/register", Register.Register);
 app.post("/login", Login.Login);
 app.post("/logout", Logout.Logout);
