8158692329a92808597df0e23344344f4f0dbcdf
Opia Precious <preciousopia7@gmail.com>
2024-07-10 23:46:05 +0100

    COMMENT

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ mongoose
     console.log(err.message);
   });
 
-//API ROUTES
+//API ENDPOINTS
 app.post("/register", Register.Register);
 app.post("/login", Login.Login);
 app.post("/logout", Logout.Logout);
