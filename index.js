9fcfbc705ea73632fe990453b5138ead201bd756
Opia Precious <preciousopia7@gmail.com>
2024-07-13 23:32:26 +0100

    Push

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ mongoose
     console.log(err.message);
   });
 
-//API ENDPOINTS
+//APPLICATION ENDPOINTS
 app.post("/register", Register.Register);
 app.post("/login", Login.Login);
 app.post("/logout", Logout.Logout);
